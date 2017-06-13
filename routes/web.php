dcfd89759f67c47a52fa6cb69664b36451dfa05e
OmarMakled <omar.makled@gmail.com>
2017-06-13 15:53:18 +0200

    Create web.php

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,14 @@
 <?php
 
-Route::get('test', function() {
-    app(ArticleRepository::class);
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('login', 'LoginController@showLoginForm');
-Route::post('login', 'LoginController@postLogin');
-Route::get('logout', 'LoginController@getLogout');
-
-Route::get('', 'ArticlesController@showArticles');
-
-Route::group(['middleware' => ['auth', 'admin']], function () {
-    Route::post('articles', 'ArticlesController@storeArticle');
-    Route::get('articles/create', 'ArticlesController@showCreateArticle');
-
-    Route::put('articles/{article}', 'ArticlesController@updateArticle');
-    Route::get('articles/{article}/edit', 'ArticlesController@showEditArticle');
-});
-
-Route::get('articles', 'ArticlesController@showArticles');
-Route::get('articles/{article}', 'ArticlesController@showArticle');
-Route::post('articles/{article}/comment', 'ArticlesController@storeComment');
-Route::get('categories/{category}', 'CategoriesController@showCategories');
-
-
+// @see App\Http\Routes\BaseRouter
